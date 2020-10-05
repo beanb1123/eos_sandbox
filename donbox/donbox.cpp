192611fa089fe197d7e92750fe281a7edec9b9d7
zzptichka <shkvorets@gmail.com>
2020-10-05 13:06:44 -0400

    added history tabel

--- donbox/donbox.cpp
+++ donbox/donbox.cpp
@@ -1,6 +1,7 @@
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
 #include <eosio/print.hpp>
+#include <eosio/system.hpp>
 #include <math.h>
 #include "include/eosio.token.hpp"
 
@@ -21,14 +22,25 @@ class [[eosio::contract("donbox")]] donbox : public eosio::contract{
       {{"SYS", 4}, "eosio.token"_n}
     };
 
-    struct [[eosio::table]] record {
+    struct [[eosio::table]] bal_record {
         eosio::name username;
         uint32_t donors;
         std::map<eosio::symbol_code, eosio::asset>  funds;
         uint64_t primary_key() const { return username.value;} 
     };
 
-    typedef eosio::multi_index< "balances"_n, record > balances;
+    typedef eosio::multi_index< "balances"_n, bal_record > balances;
+
+    struct [[eosio::table]] hist_record {
+      uint64_t id;                        //auto-inc id
+      eosio::name sender;                 
+      eosio::name receiver;
+      eosio::asset sum;
+      eosio::time_point timestamp;  
+      uint64_t primary_key() const { return id; }
+    };
+
+    typedef eosio::multi_index <"history"_n, hist_record> history;
 
 public:
     donbox(eosio::name rec, eosio::name code, datastream<const char*> ds) 
@@ -71,6 +83,15 @@ public:
           row.donors = 1;
         });
       }
+
+      history hist(get_self(), get_self().value);
+      hist.emplace( get_self(), [&]( auto& rec ) {
+        rec.id = hist.available_primary_key();
+        rec.sender = from;
+        rec.receiver = receiver;
+        rec.sum = sum;
+        rec.timestamp = current_time_point();
+      });
 
     }
     [[eosio::action]]
